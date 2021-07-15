3d4e4ab6de91dd2fb960e41c1f1b902bbf71d2ef
Asiisha Bee H <asiisha@furnify.in>
2021-07-15 22:51:10 +0530

    Initial commit

---
+++ js/shop.js
@@ -0,0 +1,483 @@
+jQuery(function () {
+    jQuery(".chair_nav").on("click", function () {
+        jQuery(".variety_product").remove();
+        $(".chair_nav").css("color", "black!important");
+        jQuery(".variety_prod_nav").after(`<section class="variety_product">
+        <div class="variety_product_divs">
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/chairjq1.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.1250</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/chairjq2.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.1650</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/chairjq3.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.850</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/chairjq4.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.1500</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+    <section class="variety_product">
+        <div class="variety_product_divs">
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/chaijq5.jpg" alt="">
+
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.2050</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/chairjq6.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.850</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/chairjq7.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.2300</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/chairjq8.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.700</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>`)
+        
+    })
+    
+})
+
+
+jQuery(function () {
+    jQuery(".sofa_nav").on("click", function () {
+        jQuery(".variety_product").remove();
+        jQuery(".variety_prod_nav").after(`<section class="variety_product">
+        <div class="variety_product_divs">
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/sofajq1.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.21,250</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/sofajq2.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.11,650</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/sofajq3.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.23,850</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/sofajq4.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.15,000</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+    <section class="variety_product">
+        <div class="variety_product_divs">
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/sofajq5.jpg" alt="">
+
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.42,050</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/sofajq6.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.19,850</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/sofajq7.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.2300</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/sofajq8.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.17,000</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>`)
+        
+    })
+    
+})
+
+
+jQuery(function () {
+    jQuery(".table_nav").on("click", function () {
+        jQuery(".variety_product").remove();
+        jQuery(".variety_prod_nav").after(`<section class="variety_product">
+        <div class="variety_product_divs">
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/sidetable1.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.2250</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/sidetable2.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.1650</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/sidetable3.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.3500</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/sidetable4.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.3100</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+    <section class="variety_product">
+        <div class="variety_product_divs">
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/sidetable5.jpg" alt="">
+
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.1450</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/sidetable6.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.1950</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/sidetable7.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.1200</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="variety_product_div">
+                <div class="prod1">
+
+                    <div class="prod1_img">
+                        <img src="../images/sidetable8.jpg" alt="">
+                    </div>
+                    <div class="prod_name">
+                        <p>Rs.1500</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>`)
+        
+    })
+    
+})
+
+
+jQuery(function () {
+    jQuery(".sales_nav").on("click", function () {
+        jQuery(".variety_product").remove();
+        jQuery(".variety_prod_nav").after(`
+        <section class="variety_product">
+            <div class="variety_product_divs">
+                <div class="variety_product_div">
+                    <div class="prod1">
+
+                        <div class="prod1_img">
+                            <img src="../images/varietyprod1.jpg" alt="">
+                        </div>
+                        <div class="prod_name">
+                            <p>Swing</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="variety_product_div">
+                    <div class="prod1">
+
+                        <div class="prod1_img">
+                            <img src="../images/varietyprod3.jpg" alt="">
+                        </div>
+                        <div class="prod_name">
+                            <p>Recliner</p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="variety_product_div">
+                    <div class="prod1">
+
+                        <div class="prod1_img">
+                            <img src="../images/1000x1000-woodeness-02-300x300 (1).jpg" alt="">
+                        </div>
+                        <div class="prod_name">
+                            <p>Bar Stool</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="variety_product_div">
+                    <div class="prod1">
+
+                        <div class="prod1_img">
+                            <img src="../images/ll1372080-s-hl3-gloflo-original-imaf9fzggzpgwkc2-3-300x300.jpeg" alt="">
+                        </div>
+                        <div class="prod_name">
+                            <p>Ceiling Light</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+
+        <section class="variety_product">
+            <div class="variety_product_divs">
+                <div class="variety_product_div">
+                    <div class="prod1">
+
+                        <div class="prod1_img">
+                            <img src="../images/brown-pvc-24002-woodness-original-imaf9rgzyk4dfwuq-300x300.jpeg" alt="">
+
+                        </div>
+                        <div class="prod_name">
+                            <p>Swing</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="variety_product_div">
+                    <div class="prod1">
+
+                        <div class="prod1_img">
+                            <img src="../images/varietyprod4.jpg" alt="">
+                        </div>
+                        <div class="prod_name">
+                            <p>Recliner</p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="variety_product_div">
+                    <div class="prod1">
+
+                        <div class="prod1_img">
+                            <img src="../images/varietyprod2.jpg" alt="">
+                        </div>
+                        <div class="prod_name">
+                            <p>Bar Stool</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="variety_product_div">
+                    <div class="prod1">
+
+                        <div class="prod1_img">
+                            <img src="../images/cooffee.jpg" alt="">
+                        </div>
+                        <div class="prod_name">
+                            <p>Ceiling Light</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>`)
+        
+    })
+    
+})
+
+
+
+jQuery(function () {
+    jQuery("a.nav-link").on("click", function () {
+       jQuery("a.nav-link").removeClass("active");
+        jQuery(this).addClass('active')
+    })
+    
+})
+
